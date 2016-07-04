949003fff8b22f9c99edeaa86a77625c1380b593
Jason Sutter <jasonsutter87@gmail.com>
2016-07-03 19:15:04 -0700

    method works execpt when it trys to call itself

--- python/reverse_string.py
+++ python/reverse_string.py
@@ -4,8 +4,8 @@ def reverse_string(string, reverse_string):
 	else:
 		first = string[0]
 		reversed_string = first + reverse_string
-		string = string[1:len(string)]
+		string = string[1:]
 		print string
 		print reversed_string
-		# reverse_string(string, reversed_string)
+		reverse_string(string, reversed_string)
 reverse_string("jason", "")
